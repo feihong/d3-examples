d4d4363713a9c81cfca5aa05f6c4f7c9f7895fb4
Feihong Hsu <feihong.hsu@gmail.com>
2015-12-31 12:06:53 -0600

    Add site structure

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+
+
+module.exports = {
+  entry: {
+    'hello-world': './site/hello-world/app.es6',
+    // This comment marks where new entry points will be added
+  },
+  output: {
+    path: path.resolve('site'),
+    filename: '[name]/bundle.js'
+  },
+  resolve: {
+    alias: {
+      lib: path.resolve('site/lib')
+    },
+    extensions: ['', '.webpack.js', '.web.js', '.js', '.es6']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.es6$/,
+        include: [
+          path.resolve('site')
+        ],
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015'],
+          cacheDirectory: true
+        }
+      }
+    ]
+  }
+}
